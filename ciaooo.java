e35a21f46646a5c57bb904d58cc08b77b2810f9d
Moksh Gulati <mokshgulati14@gmail.com>
2020-08-16 17:13:22 +0530

    jhjsdf

--- ciaooo.java
+++ ciaooo.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ciaooo");
+        System.out.println("GoodBye");
+        System.out.println("Hello");
     }
 }
